e553447cf503affa9716bd7fe2b4fed13da5a22e
WillsMcGraw <willsmcgraw21@vt.edu>
2024-09-14 00:23:31 -0400

    Add code to send emails to user

--- email_script.py
+++ email_script.py
@@ -1,26 +1,48 @@
 import weather_script
+import json
+import ssl
 import smtplib
-from email.mime.text import MIMEText
+from email.message import EmailMessage
 
 # Grabs temperature data from weather_script.py
 data = weather_script.main()
 
-# Prints temperature data onto a text file and converts it
-# into MIMEText
-with open('output.txt', 'w') as my_file:
-    # Prints temperature data onto the output file
-    for i in range(5):
-        my_file.write(f'Temperature: {data['act_temp'][i]}\n')
-        my_file.write(f'Feels like: {data['feel_temp'][i]}\n\n')
-    # Converts the text file into MIMEText
-    email = MIMEText(my_file.read())
-    email['Subject'] = my_file.read()
-    # Closes output file
-    my_file.close()
+# Creates an empty dictionary for config variables to be stored in
+env_config = {}
 
-email['From'] = "wills.mcgraw21@gmail.com"
-email['To'] = "wills.mcgraw21@gmail.com"
+# Opens the JSON files holding the config information and stores them
+# in the env_config dictionary
+with open('email_config.json', 'r') as config_json:
+    config = json.load(config_json)
+    env_config['key'] = config['key']
+    env_config['sender'] = config['sender']
+    env_config['receiver'] = config['receiver']
 
-s = smtplib.SMTP('localhost')
-s.sendmail("wills.mcgraw21@gmail.com", ["wills.mcgraw21@gmail.com"], email.as_string())
-s.quit()
+# Renames the env_config variables into global variables for ease of use
+KEY = env_config['key']
+SENDER = env_config['sender']
+RECEIVER = env_config['receiver']
+
+# Saves the email subject line in a variable to be called later
+email_subject = 'Morning Weather Update'
+
+# Saves the email body in a variable to be called later
+email_body_parts = []
+for i in range(len(data['act_temp'])):
+    email_body_parts.append(f'Temperature of {data['act_temp'][i]}; Feels like {data['feel_temp'][i]}')
+email_body = '\n'.join(email_body_parts)
+
+# Creates and sets email settings
+email = EmailMessage()
+email['From'] = SENDER
+email['To'] = RECEIVER
+email['Subject'] = email_subject
+email.set_content(email_body)
+
+# Creates context to improve email security
+context = ssl.create_default_context()
+
+# Logs into gmail account to send morning weather update
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(SENDER, KEY)
+    smtp.sendmail(SENDER, RECEIVER, email.as_string())
