6544099ba40be4aefb02de63fe5a7eb3b020f710
WillsMcGraw <willsmcgraw21@vt.edu>
2024-09-14 01:02:14 -0400

    Edit body of email to include a time and date for the email update

--- email_script.py
+++ email_script.py
@@ -17,19 +17,27 @@ with open('config.json', 'r') as config_json:
     env_config['key'] = config['email_key']
     env_config['sender'] = config['sender']
     env_config['receiver'] = config['receiver']
+    env_config['city'] = config['city']
+    env_config['state_code'] = config['state_code']
 
 # Renames the env_config variables into global variables for ease of use
 KEY = env_config['key']
 SENDER = env_config['sender']
 RECEIVER = env_config['receiver']
+CITY = env_config['city']
+STATE = env_config['state_code']
 
 # Saves the email subject line in a variable to be called later
 email_subject = 'Morning Weather Update'
 
 # Saves the email body in a variable to be called later
 email_body_parts = []
+email_body_parts.append(f'Morning weather update for {CITY}, {STATE} on {data['time'][0].strftime('%Y-%m-%d')}')
 for i in range(len(data['act_temp'])):
-    email_body_parts.append(f'Temperature of {data['act_temp'][i]}; Feels like {data['feel_temp'][i]}')
+    a_temp = data['act_temp'][i]
+    f_temp = data['feel_temp'][i]
+    time = data['time'][i].strftime('%H-%M')
+    email_body_parts.append(f'\t{time}: {a_temp} F; feels like {f_temp} F')
 email_body = '\n'.join(email_body_parts)
 
 # Creates and sets email settings
